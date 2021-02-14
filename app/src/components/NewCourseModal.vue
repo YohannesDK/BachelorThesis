244964530b50aa44f34f2f808200ccfbdb7896e4
Yohannes DK <kassaye85@gmail.com>
2021-02-14 03:29:25 +0100

    need to fix later

--- app/src/components/NewCourseModal.vue
+++ app/src/components/NewCourseModal.vue
@@ -1,16 +1,48 @@
 <template>
   <h1>halla</h1>
+
+  <div class="modal fade in modal-active">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" @click="$emit('close')" class="close">
+            <span>&times;</span>
+          </button>
+          <h4 class="modal-title">
+            {{ name }}
+          </h4>
+        </div>
+        <div class="modal-body">
+          <slot></slot>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" @click="$emit('close')">
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
 </template>
 <script lang="ts">
-import {defineComponent, onMounted} from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "NewCourseModal",
-  setup() {
-    onMounted(() => {
-      console.log("Hello world");
-    })
-  }
-})
-
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+  },
+  emits: ["close"]
+});
 </script>
+
+<style scoped>
+.modal-active {
+  display: block;
+}
+</style>
--- app/src/views/Courses.vue
+++ app/src/views/Courses.vue
@@ -1,3 +1,35 @@
 <template>
   <h1>This is the courses page</h1>
+  <button class="btn btn-primary" @click="ToogleModal()">Toogle Model</button>
+  <button class="btn btn-secondary" @click="showModal=true">Show</button>
+ 
+  <new-course-modal v-if="showModal" @close="showModal = false" name="Umesh">
+    <p>dsfkdskf</p>
+  </new-course-modal>
 </template>
+
+
+<script lang="ts">
+// https://codepen.io/umeshagouda/pen/QggMve
+import { defineComponent, ref } from "vue";
+import NewCourseModal from "@/components/NewCourseModal.vue";
+
+export default defineComponent({
+  name: "Courses",
+  components: {
+    NewCourseModal
+  },
+  setup() {
+    const showModal = ref<boolean>(true);
+    const ToogleModal = () => {
+      showModal.value = !showModal.value;
+      console.log(showModal.value);
+    };
+
+    return {
+      showModal,
+      ToogleModal
+    };
+  }
+});
+</script>
