b118d6c8297cf10b43a36b528cfb59e660dd1a1b
YohannesDK <kassaye85@gmail.com>
2021-02-17 17:14:59 +0100

    pray

---
+++ app/test/unit/example.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from "chai";
+// import { shallowMount } from "@vue/test-utils";
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+// describe("HelloWorld.vue", () => {
+//   it("renders props.msg when passed", () => {
+//     const msg = "new message";
+//     const wrapper = shallowMount(HelloWorld, {
+//       props: { msg }
+//     });
+//     expect(wrapper.text()).to.include(msg);
+//   });
+// });
+
+describe("Dummy test frontend", () => {
+  let result;
+  it("Should return 5", () => {
+    result = 2+3;
+    expect(result).to.equal(5);
+  });
+  console.log(result);
+});
