2b3ce6b6dee572d6fc42ec762d1abd053dda2900
YohannesDK <kassaye85@gmail.com>
2021-02-22 08:20:09 +0100

    add react and react_dom for typeform

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -2,26 +2,14 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>Home Page</h1>
-    <document-card :document="document" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import DocumentCard from "@/components/documentCard.vue";
-import store from "@/store";
 
 export default defineComponent({
   name: "Home",
-  components: {
-    DocumentCard
-  },
-  setup() {
-    const document = store.getters.getDocmentbyId(0);
-    return {
-      document
-    };
-  }
 });
 </script>
 
